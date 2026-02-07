908534855487d5fe7827d0c4856abab5fc6796a2
changsiuuuu <changsiuuuu@gmail.com>
2026-02-07 22:06:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -126,7 +126,15 @@ const darkToggle = document.getElementById("darkToggle");
 
 darkToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    darkToggle.textContent = "☀️";
+  } else {
+    darkToggle.textContent = "🌙";
+  }
 });
+
+
 
 
 
